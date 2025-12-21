0e6aff4d52770a3089034b27b63b7fdb3fc9c2ee
Herpinho <149377061+Herpinho@users.noreply.github.com>
2025-12-21 19:42:06 +0000

    Update schema.sql

--- loan_service/schema.sql
+++ loan_service/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS loans (
     loan_id SERIAL PRIMARY KEY,
-    copy_id INTEGER NOT NULL,  -- Links to a specific book copy
-    user_id INTEGER NOT NULL,  -- Links to the user from User Service
+    copy_id varchar(50),
+    user_id INTEGER NOT NULL,
     loan_date DATE DEFAULT CURRENT_DATE,
     due_date DATE NOT NULL,
     return_date DATE,
